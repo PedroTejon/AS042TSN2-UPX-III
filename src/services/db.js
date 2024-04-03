5c99d8c7dc4e288468f95b2e4a921a2433d22f91
PedroTejon <pedrohtejon@gmail.com>
2024-04-02 23:49:36 -0300

    feat: serviço básico para o acesso ao banco de dados

---
+++ src/services/db.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2/promise');
+
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'upxiii',
+    connectTimeout: 60000,
+  });
+
+  return connection;
+}
+
+async function query(sql, params, connection) {
+  const [results,] = await connection.execute(sql, params);
+
+  return results;
+}
+
+module.exports = {
+  query,
+  getConnection,
+};
