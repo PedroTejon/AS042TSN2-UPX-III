8bfbfb1888dbc6ac7ef53f8c0fe7aba0183a377f
PedroTejon <pedrohtejon@gmail.com>
2024-04-03 00:02:07 -0300

    fix: atualização do script de banco de dados, não estava totalmente correto anteriormente

--- Script Setup BD - MySQL.sql
+++ Script Setup BD - MySQL.sql
@@ -1,3 +1,5 @@
+DROP DATABASE UPXIII;
+CREATE DATABASE UPXIII;
 USE UPXIII;
 
 CREATE TABLE IF NOT EXISTS plataformas (
@@ -7,13 +9,14 @@ CREATE TABLE IF NOT EXISTS plataformas (
   PRIMARY KEY (id_plataforma)
 );
 
-CREATE TABLE IF NOT EXISTS anuncios (
+CREATE TABLE  IF NOT EXISTS anuncios (
   id_anuncio INT NOT NULL AUTO_INCREMENT,
   titulo VARCHAR(150) NOT NULL,
   avaliacao DECIMAL(4,2) NULL,
   preco FLOAT NOT NULL,
+  descricao TEXT NULL,
   URL VARCHAR(200) NOT NULL,
-  foto BLOB NOT NULL,
+  foto VARCHAR(300) NOT NULL,
   id_plataforma INT NOT NULL,
   PRIMARY KEY (id_anuncio),
   FOREIGN KEY (id_plataforma) REFERENCES plataformas (id_plataforma)
@@ -45,44 +48,40 @@ CREATE TABLE IF NOT EXISTS anuncios_escondidos (
   FOREIGN KEY (id_anuncio) REFERENCES anuncios (id_anuncio)
 );
 
+INSERT INTO plataformas VALUES (null, 'EnergiaTotal', 'https://www.energiatotal.com.br');
+INSERT INTO plataformas VALUES (null, 'Leroy Merlin', 'https://www.leroymerlin.com.br/');
+INSERT INTO plataformas VALUES (null, 'Magazine Luiza', 'https://www.magazineluiza.com.br/');
+INSERT INTO plataformas VALUES (null, 'Mercado Livre', 'https://www.mercadolivre.com.br/');
+INSERT INTO plataformas VALUES (null, 'Minha Casa Solar', 'https://www.minhacasasolar.com.br/');
+INSERT INTO plataformas VALUES (null, 'NeoSolar', 'https://www.neosolar.com.br/loja');
+
 DELIMITER $$
-DROP PROCEDURE IF EXISTS insert_plat;
-CREATE PROCEDURE insert_plat(nome VARCHAR(100), URL VARCHAR(100))
+CREATE PROCEDURE insert_anun(titulo VARCHAR(150), avaliacao DECIMAL(4,2), preco FLOAT, descricao TEXT, URL VARCHAR(200), foto VARCHAR(300), plat INT)
 BEGIN
-  INSERT INTO plataformas VALUES (nome, URL);
+  INSERT INTO anuncios VALUES (default, titulo, avaliacao, preco, descricao, URL, foto, plat);
 END$$
 
-DROP PROCEDURE IF EXISTS insert_anun;
-CREATE PROCEDURE insert_anun(titulo VARCHAR(150), avaliacao DECIMAL(4,2), preco FLOAT, URL VARCHAR(200), foto BLOB)
-BEGIN
-  INSERT INTO anuncios VALUES (titulo, avaliacao, preco, URL, foto);
-END$$
-
-DROP PROCEDURE IF EXISTS insert_user;
 CREATE PROCEDURE insert_user(email VARCHAR(75), senha_hash VARCHAR(200), nome VARCHAR(45), data_nasc DATE, genero VARCHAR(45))
 BEGIN
-  INSERT INTO usuarios VALUES (email, senha_hash, nome, data_nasc, genero);
+  INSERT INTO usuarios VALUES (default, email, senha_hash, nome, data_nasc, genero);
 END$$
 
-DROP PROCEDURE IF EXISTS hide_anun;
 CREATE PROCEDURE hide_anun(id_usuario INT, id_anuncio INT)
 BEGIN
   INSERT INTO anuncios_escondidos VALUES (id_usuario, id_anuncio);
 END$$
 
-DROP PROCEDURE IF EXISTS save_anun;
 CREATE PROCEDURE save_anun(id_usuario INT, id_anuncio INT)
 BEGIN
   INSERT INTO anuncios_salvos VALUES (id_usuario, id_anuncio);
 END$$
 
-DROP PROCEDURE IF EXISTS query_page;
 CREATE PROCEDURE query_page(index_i INT) 
 BEGIN
   SELECT * FROM anuncios 
   LEFT JOIN anuncios_escondidos ON anuncios.id_anuncio = anuncios_escondidos.id_anuncio
   WHERE anuncios_escondidos.id_anuncio = NULL 
   LIMIT index_i OFFSET 50;
-END$$
+END $$
 
 DELIMITER ;
