ae9c0b8fd893be40909170ff3a38900169f6468a
PedroTejon <pedrohtejon@gmail.com>
2024-04-10 22:14:41 -0300

    fix: ajuste da função global de error handling

--- src/app.js
+++ src/app.js
@@ -25,11 +25,11 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.message = err.message;
+  res.error = err;
 
-  res.status(err.status || 50);
+  res.status(500);
+  res.send(err);
 });
 
 module.exports = app;
