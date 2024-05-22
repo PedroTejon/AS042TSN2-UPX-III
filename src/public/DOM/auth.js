637c0bb237508c95f49e591c9052b80457880095
PedroTejon <pedrohtejon@gmail.com>
2024-05-21 22:11:10 -0300

    feat: auth de sessão

--- src/public/DOM/auth.js
+++ src/public/DOM/auth.js
@@ -14,6 +14,44 @@ function getCookie(cname) {
     return "";
 }
   
-// if (getCookie('userId') && getCookie('sessionHash')) {
-
-// }
+if (getCookie('userId')) {
+  fetch('/api/users/').then(response => response.json()).then(data => {
+    if (data.email) {
+      const profileContainer = document.querySelector('.profile-info');
+      profileContainer.innerHTML = `<div class="pfp-div">
+      <img src="../assets/common/pfp-icon.png" alt="Foto de perfil padrão" class="unselectable" draggable="false" >
+  </div>
+  <div class="userinfo-div unselectable" draggable="false" >
+      <p id="username">${data.name}</p>
+      <a id="see-profile" draggable="false" href="/profile">Ver meu perfil</a>
+  </div>`
+    }
+    // if (data.error) {
+    //   console.log(data.error);
+    // } else {
+    //   let user = data.user;
+    //   let name = document.getElementById('name');
+    //   let email = document.getElementById('email');
+    //   let phone = document.getElementById('phone');
+    //   let cpf = document.getElementById('cpf');
+    //   let birthdate = document.getElementById('birthdate');
+    //   let address = document.getElementById('address');
+    //   let city = document.getElementById('city');
+    //   let state = document.getElementById('state');
+    //   let country = document.getElementById('country');
+    //   let zip = document.getElementById('zip');
+    //   let products = document.getElementById('products');
+    //   name.value = user.name;
+    //   email.value = user.email;
+    //   phone.value = user.phone;
+    //   cpf.value = user.cpf;
+    //   birthdate.value = user.birthdate;
+    //   address.value = user.address;
+    //   city.value = user.city;
+    //   state.value = user.state;
+    //   country.value = user.country;
+    //   zip.value = user.zip;
+    //   products.value = user.products;
+    // }
+  })
+}
