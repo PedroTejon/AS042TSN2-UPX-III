8c520c19bd850346e420e27bc1beb0b0ce4f9131
PedroTejon <pedrohtejon@gmail.com>
2024-05-21 23:06:21 -0300

    feat: integração de API no registro de usuário

--- src/public/DOM/signup.js
+++ src/public/DOM/signup.js
@@ -1,3 +1,30 @@
-function signup() {
-    
+function signup(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('username').value;
+    const email = document.getElementById('usermail').value;
+    const password = document.getElementById('userpass').value;
+    const birthDate = document.getElementById('userbday').value;
+    const gender = document.getElementById('usergender').value;
+
+    debugger
+
+    fetch('/api/users/register', {
+        method: 'POST',
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+            gender: gender,
+            birthDate: birthDate
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    ).then(response => response.json()).then(data => {
+        if (data.message == 'Usuário criado com sucesso!') {
+            window.location.href = '/login'
+        }
+    })
 }
